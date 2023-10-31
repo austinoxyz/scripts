4df988b8437ac89743eedd29a0f1801659503ac0
Austin O'Donnell <aodonnell2536.3@gmail.com>
2023-10-31 00:43:57 -0400

    move bookmark category selection common function

--- utility/common.sh
+++ utility/common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage fatal_script_error [message]
+# Usage: fatal_script_error [message]
 fatal_script_error() {
     if [ $# -ne 1 ]; then exit 1; fi
     echo "$1" >&2
@@ -8,11 +8,34 @@ fatal_script_error() {
     exit 1
 }
 
-# Usage load_dmenu_config
+# Usage: load_dmenu_config
 load_dmenu_config() {
     dmenu_config=${XDG_CONFIG_HOME:-$HOME/.config}/dmenu
     if [ ! -e "${dmenu_config}" ]; then
         fatal_script_error "dmenu config not found at '$(dmenu_config)'"
     fi
     . ${dmenu_config}
+}
+
+# Usage: select_bookmark_file
+select_bookmark_file() {
+    categories="$(echo "$(ls -1 ${BOOKMARKS_DIR})" | sed 's/\.[^.]*$//')"
+
+    if [ -z "${categories}" ]; then
+        fatal_script_error "No bookmark files found in directory '${BOOKMARKS_DIR}'."
+    fi
+
+    selected_category="$(echo "${categories}" | dmenu -F -c -i \
+        -h 30                  \
+        -bw ${border_width}    \
+        -l 15                  \
+        -m ${monitor}          \
+        -p "select category:"  \
+        -fn ${font})"
+
+    if [ -z ${selected_category} ]; then
+        fatal_script_error "Category selection is empty."
+    fi
+
+    echo "${BOOKMARKS_DIR}/${selected_category}.txt"
 }
