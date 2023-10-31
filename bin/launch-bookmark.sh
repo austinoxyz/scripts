3d2c5abba3157e1ed74ea3bbaca224585e0c4bc8
Austin O'Donnell <aodonnell2536.3@gmail.com>
2023-10-31 00:11:11 -0400

    rename everything

--- bin/launch-bookmark.sh
+++ bin/launch-bookmark.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
-. ${XDG_CONFIG_HOME:-$HOME/.config}/dmenu
+. ${SCRIPTS}/utility/common.sh
+
+load_dmenu_config
+
 BOOKMARKS_DIR=${XDG_DATA_HOME:-$HOME/.local/share}/bookmarks
 
-selected_category=$(select_bookmark_category)
-[ -z ${selected_category} ] && exit 0
+if [ ! -d ${BOOKMARKS_DIR} ]; then
+    fatal_script_error "Bookmarks directory not found."
+fi
+
+categories="$(echo "$(ls -1 ${BOOKMARKS_DIR})" | sed 's/\.[^.]*$//')"
+
+if [ -z "${categories}" ]; then
+    fatal_script_error "No bookmark files found in directory '${BOOKMARKS_DIR}'."
+fi
+
+selected_category="$(echo "${categories}" | dmenu -F -c -i \
+    -h 30                  \
+    -bw ${border_width}    \
+    -l 15                  \
+    -m ${monitor}          \
+    -p "select category:"  \
+    -fn ${font})"
+
+if [ -z ${selected_category} ]; then
+    fatal_script_error "Category selection is empty."
+fi
 
 bookmarks_file=${BOOKMARKS_DIR}/${selected_category}.txt
+
 selected_bookmark="$(cat ${bookmarks_file} | dmenu -F -c -i \
     -h 30                                                   \
-    -bw $border_width                                       \
+    -bw ${border_width}                                     \
     -l 15                                                   \
-    -m $monitor                                             \
+    -m ${monitor}                                           \
     -p "select site:"                                       \
-    -fn $font)"
+    -fn ${font})"
 
 # for some reason when selecting multiple bookmarks, wc is counting 1 as 1, 2 as 3, 3 as 4, and so on.
 n_selected=$(echo $selected_bookmark | wc --words - | cut -d' ' -f 1)
