326c41760fdbc77518feef751f4b2c417ed26ab3
austinoxyz <aodonnell2536.3@gmail.com>
2024-04-17 22:26:43 -0400

    Move keyboard configuration into a single script.

---
+++ utility/config-keyboard.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+# config xset to have lower repeat rate 
+# for faster typing
+xset r rate 200 40 m 0 0
+
+# swap escape and capslock
+setxkbmap -option caps:swapescape
+
+notify-send "hello"
