e005c45536ec290169f61f777330eb3d574c8538
austinoxyz <aodonnell2536.3@gmail.com>
2024-04-17 23:06:53 -0400

    rename

--- utility/setup-monitors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --primary --left-of DP-4 \
-           --mode 1920x1080 --rate 144.00 --brightness 1 --gamma 1:1:1 \
-       --output DP-4 --crtc 1 \
-           --mode 1920x1080 --rate  60.00 --brightness 1 --gamma 1:1:1 
